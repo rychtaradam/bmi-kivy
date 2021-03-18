b70d55822889c815e6effdfab35dfe2f7e167b5d
Adam Rychtář <rychtar.adamek@seznam.cz>
2021-03-18 12:54:36 +0100

    Znovu vytvoření projektu - omylem jsem ho smazal

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from modules.persons import Persons
 
 
-class TextScreen(Screen):
+class BMIScreen(Screen):
     pass
 
 
@@ -23,7 +24,12 @@ class Test(MDApp):
 
     def build(self):
         self.theme_cls.primary_palette = "Gray"
-        return Builder.load_file('bmi.kv')
+        builder = Builder.load_file('bmi.kv')
+
+        persons = Persons()
+        builder.ids.navigation.ids.tab_manager.screens[2].add_widget(persons)
+
+        return builder
 
 
 Test().run()

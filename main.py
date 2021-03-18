993f09fd1bae456bd2d37aa7d98ce623dd5da1d0
Adam Rychtář <rychtar.adamek@seznam.cz>
2021-03-18 10:24:16 +0100

    Start commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from kivy.uix.screenmanager import Screen
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+
+class TextScreen(Screen):
+    pass
+
+
+class FormScreen(Screen):
+    pass
+
+
+class TableScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
+    pass
+
+
+class Test(MDApp):
+
+    def build(self):
+        self.theme_cls.primary_palette = "Gray"
+        return Builder.load_file('bmi.kv')
+
+
+Test().run()
